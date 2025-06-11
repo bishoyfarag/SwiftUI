d83258f6ed7c2b8c912388e21c1addbbb28f52ac
bishoy-farag1 <bishoy.maged95@gmail.com>
2025-06-10 19:27:57 -0700

    Add Better Rest Project

---
+++ Better Rest/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by csuftitan on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
