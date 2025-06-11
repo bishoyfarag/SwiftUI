db0b0ba7310e93d9415cb82c5a67112bcc1fc15e
bishoy-farag1 <bishoy.maged95@gmail.com>
2025-06-10 19:32:41 -0700

    Add Word Scrumble Project

---
+++ Word Scrumble/WordScrumble/WordScrumble/WordScrumbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrumbleApp.swift
+//  WordScrumble
+//
+//  Created by csuftitan on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrumbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
